4b140bf1befa50807c4abd08d22580d0a7ede975
Patrick Carver <patrickcarver@gmail.com>
2020-12-01 13:31:23 -0600

    Day 01

---
+++ day01/lib/day01.ex
@@ -0,0 +1,52 @@
+defmodule Day01 do
+  def part1 do
+    ints = ints("priv/input.txt")
+    set = MapSet.new(ints)
+    {a, b} = find_pair(ints, set, 2020)
+    a * b
+  end
+
+  def part2 do
+    ints = ints("priv/input.txt")
+    set = MapSet.new(ints)
+    {a, b, c} = find_triplet(ints, set, 2020)
+    a * b * c
+  end
+
+  def ints(file_name) do
+    file_name
+    |> File.stream!()
+    |> Enum.map(fn line ->
+      line
+      |> String.trim_trailing()
+      |> String.to_integer()
+    end)
+  end
+
+  def find_pair([], _set, _target) do
+    :no_pairs_found
+  end
+
+  def find_pair([head | tail], set, target) do
+    diff = target - head
+
+    if MapSet.member?(set, diff) do
+      {head, diff}
+    else
+      find_pair(tail, set, target)
+    end
+  end
+
+  def find_triplet([head | tail], set, target) do
+    diff = target - head
+
+    result = find_pair(tail, set, diff)
+
+    if result == :no_pairs_found do
+      find_triplet(tail, set, target)
+    else
+      {a, b} = result
+      {a, b, head}
+    end
+  end
+end
---
+++ day01/test/day01_test.exs
@@ -0,0 +1,8 @@
+defmodule Day01Test do
+  use ExUnit.Case
+  doctest Day01
+
+  test "greets the world" do
+    assert Day01.hello() == :world
+  end
+end
