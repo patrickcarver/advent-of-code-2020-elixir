6e78664c91b0c709ea8ae196f59e07de5e69b897
Patrick Carver <patrickcarver@gmail.com>
2020-12-05 01:20:28 -0600

    Day05 complete

--- day05/lib/day05.ex
+++ day05/lib/day05.ex
@@ -1,22 +1,61 @@
 defmodule Day05 do
   def part1 do
     "priv/input.txt"
+    |> taken_seats()
+    |> highest_seat_id()
+  end
+
+  def part2 do
+    "priv/input.txt"
+    |> taken_seats()
+    |> my_seat()
+  end
+
+  def taken_seats(file_name) do
+    file_name
+    |> stream()
+    |> seats()
+  end
+
+  def my_seat(taken_seats) do
+    all_seats = all_seats(taken_seats)
+
+    hd(all_seats -- taken_seats)
+  end
+
+  def all_seats(taken_seats) do
+    taken_seats
+    |> Enum.min_max()
+    |> (fn {min, max}-> Enum.to_list(min..max) end).()
+  end
+
+  def seats(stream) do
+    Enum.map(stream, &seat_id/1)
+  end
+
+  def highest_seat_id(seats) do
+    Enum.max(seats)
+  end
+
+  def stream(file_name) do
+    file_name
     |> File.stream!()
-    |> Stream.map(& &1 |> String.trim_trailing() |> parse() |> seat_id())
-    |> Enum.max()
+    |> Stream.map(&String.trim_trailing/1)
   end
 
-  def seat_id({row_number, column_number}) do
-    row_number * 8 + column_number
-  end
-
-  def parse(boarding_pass) do
-    {row, column} = boarding_pass |> String.graphemes() |> Enum.split(7)
+  def seat_id(boarding_pass) do
+    {row, column} = rows_and_columns(boarding_pass)
 
     row_number = convert(row, {0, 127})
     column_number = convert(column, {0, 7})
 
-    {row_number, column_number}
+    row_number * 8 + column_number
+  end
+
+  def rows_and_columns(boarding_pass) do
+    boarding_pass
+    |> String.graphemes()
+    |> Enum.split(7)
   end
 
   def convert(letters, range) do
@@ -39,9 +78,5 @@ defmodule Day05 do
   def do_convert(value, {min, max}) when value in ~w[B R] do
     bottom = div((max + min), 2) + 1
     {bottom, max}
-  end
-
-  def part2 do
-    nil
   end
 end
