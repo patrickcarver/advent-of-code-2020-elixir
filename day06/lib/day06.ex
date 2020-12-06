919eb79d3ae236ad408c3576e8943d547fd65c46
Patrick Carver <patrickcarver@gmail.com>
2020-12-06 03:42:51 -0600

    Day 06 complete

--- day06/lib/day06.ex
+++ day06/lib/day06.ex
@@ -3,15 +3,29 @@ defmodule Day06 do
     "priv/input.txt"
     |> File.read!()
     |> String.split("\n\n", trim: true)
-    |> Enum.reduce(0, fn line, acc ->
-      group_count =
-        line
-        |> String.replace("\n", "")
-        |> String.graphemes()
-        |> Enum.uniq()
-        |> length()
+    |> Enum.reduce(0, fn line, count ->
+      line
+      |> String.replace("\n", "")
+      |> String.graphemes()
+      |> Enum.uniq()
+      |> length()
+      |> Kernel.+(count)
+    end)
+  end
 
-      acc + group_count
+  def part2 do
+    "priv/input.txt"
+    |> File.read!()
+    |> String.split("\n\n", trim: true)
+    |> Enum.reduce(0, fn line, count ->
+      line
+      |> String.split("\n", trim: true)
+      |> Enum.reduce(MapSet.new(?a..?z), fn answers, acc ->
+        answers = answers |> to_charlist() |> MapSet.new()
+        MapSet.intersection(answers, acc)
+      end)
+      |> MapSet.size()
+      |> Kernel.+(count)
     end)
   end
 end
