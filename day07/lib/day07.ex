63ad5e84ff695a3ffdf1b9cfb09c1918ef5b2e98
Patrick Carver <patrickcarver@gmail.com>
2020-12-07 16:00:04 -0600

    Day 07 complete

--- day07/lib/day07.ex
+++ day07/lib/day07.ex
@@ -2,22 +2,41 @@ defmodule Day07 do
   def part1 do
     "priv/input.txt"
     |> stream()
-    |> Stream.map(fn line ->
+    |> parse_lines()
+    |> children_to_parent_map
+    |> count_bags_that_eventually_contain_shiny_gold()
+  end
+
+  def part2 do
+    "priv/input.txt"
+    |> stream()
+    |> parse_lines()
+    |> parent_to_children_map()
+    |> count_bags_inside_shiny_gold()
+  end
+
+  def parse_lines(stream) do
+    Stream.map(stream, fn line ->
       [parent, children] = line |> String.replace([" bags", " bag"], "") |> String.split(" contain ")
       children = children |> String.trim_trailing(".") |> String.split(", ", trim: true) |> Enum.map(&parse_child/1)
 
       {parent, children}
     end)
-    |> Enum.reduce(%{}, fn {parent, children}, bags ->
-      Enum.reduce(children, bags, fn {color, quantity}, acc ->
-        Map.update(acc, color, [{parent, quantity}], & [{parent, quantity} | &1]) |> Map.put_new(parent, [])
-      end)
-    end)
-    |> count_bags_that_eventually_contain_target("shiny gold")
   end
 
-  def part2 do
-    "priv/input.txt"
+  def children_to_parent_map(stream) do
+    Enum.reduce(stream, %{}, fn {parent, children}, bags ->
+      Enum.reduce(children, bags, fn {color, quantity}, acc ->
+        acc
+        |> Map.update(color, [{parent, quantity}], & [{parent, quantity} | &1])
+        |> Map.put_new(parent, [])
+      end)
+    end)
+  end
+
+  def parent_to_children_map(stream) do
+    stream
+    |> Map.new()
   end
 
   def stream(file_name) do
@@ -34,14 +53,30 @@ defmodule Day07 do
     {color, String.to_integer(quantity)}
   end
 
-  def count_bags_that_eventually_contain_target(map, target) do
-    visit(map, target, MapSet.new()) |> MapSet.size()
+  def count_bags_inside_shiny_gold(map) do
+    count_bags_inside(map, map["shiny gold"]) - 1
   end
 
-  def visit(map, target, visited) do
-     Map.get(map, target) |> Enum.reduce(visited, fn {color, _}, acc ->
+  def count_bags_inside(_map, [{"no other", 0}]) do
+    1
+  end
+
+  def count_bags_inside(map, children) do
+    Enum.reduce(children, 1, fn {color, quantity}, acc -> acc + quantity * count_bags_inside(map, map[color]) end)
+  end
+
+  def count_bags_that_eventually_contain_shiny_gold(map) do
+    map
+    |> visit_parents("shiny gold", MapSet.new())
+    |> MapSet.size()
+  end
+
+  def visit_parents(map, target, visited) do
+     map
+     |> Map.get(target)
+     |> Enum.reduce(visited, fn {color, _}, acc ->
       new_acc = MapSet.put(acc, color)
-      visit(map, color, new_acc)
+      visit_parents(map, color, new_acc)
     end)
   end
 end
