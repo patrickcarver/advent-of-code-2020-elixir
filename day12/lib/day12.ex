24bddd271cf34ae4345a431acc5883a8d1528970
Patrick Carver <patrickcarver@gmail.com>
2020-12-12 09:05:37 -0600

    Day 12 Part 2 complete

---
+++ day12/lib/day12.ex
@@ -0,0 +1,67 @@
+defmodule Day12 do
+  @orientations ~w[north east south west]a
+
+  def part1 do
+    "priv/input.txt"
+    |> actions()
+    |> navigation()
+    |> distance()
+  end
+
+  def distance(%{x: x, y: y}) do
+    manhattan_distance({x, y}, {0, 0})
+  end
+
+  def navigation(actions) do
+    Enum.reduce(actions, new_ferry(), &act/2)
+  end
+
+  def new_ferry() do
+    %{x: 0, y: 0, orientation: :east}
+  end
+
+  def act({:north, value}, %{y: y} = ferry), do: %{ferry | y: y + value}
+  def act({:south, value}, %{y: y} = ferry), do: %{ferry | y: y - value}
+  def act({:east, value},  %{x: x} = ferry), do: %{ferry | x: x + value}
+  def act({:west, value},  %{x: x} = ferry), do: %{ferry | x: x - value}
+
+  def act({:right, value}, %{orientation: ferry_orientation} = ferry) do
+    new_orientation = new_orientation(ferry_orientation, value)
+    %{ferry | orientation: new_orientation}
+  end
+
+  def act({:left, 90}, ferry), do: act({:right, 270}, ferry)
+  def act({:left, 270}, ferry), do: act({:right, 90}, ferry)
+  def act({:left, value}, ferry), do: act({:right, value}, ferry)
+
+  def act({:forward, value}, %{orientation: orientation} = ferry) do
+    act({orientation, value}, ferry)
+  end
+
+  def new_orientation(ferry_orientation, value) do
+    index = Enum.find_index(@orientations, fn orientation -> orientation == ferry_orientation end)
+    target = rem(index + div(value, 90), 4)
+    Enum.at(@orientations, target)
+  end
+
+  def actions(file) do
+    file
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1)
+    |> Stream.map(&parse/1)
+  end
+
+  def manhattan_distance({target_x, target_y}, {origin_x, origin_y}) do
+    abs(target_x - origin_x) + abs(target_y - origin_y)
+  end
+
+  def parse("N" <> value), do: {:north, String.to_integer(value)}
+  def parse("E" <> value), do: {:east, String.to_integer(value)}
+  def parse("S" <> value), do: {:south, String.to_integer(value)}
+  def parse("W" <> value), do: {:west, String.to_integer(value)}
+
+  def parse("R" <> value), do: {:right, String.to_integer(value)}
+  def parse("L" <> value), do: {:left, String.to_integer(value)}
+
+  def parse("F" <> value), do: {:forward, String.to_integer(value)}
+end
---
+++ day12/test/day12_test.exs
@@ -0,0 +1,8 @@
+defmodule Day12Test do
+  use ExUnit.Case
+  doctest Day12
+
+  test "greets the world" do
+    assert Day12.hello() == :world
+  end
+end
